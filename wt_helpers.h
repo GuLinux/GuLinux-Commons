98c9b6bcf55eac754d31197859f44fb6beebeca2
Marco Gulino <marco@gulinux.net>
2013-06-08 19:41:52 +0200

    Fix license

--- wt_helpers.h
+++ wt_helpers.h
@@ -1,3 +1,23 @@
+/***********************************************************************
+Copyright (c) 2013 "Marco Gulino <marco.gulino@gmail.com>"
+
+This file is part of Pandorica: https://github.com/GuLinux/Pandorica
+
+Pandorica is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (included the COPYING file).
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
+
+
 #ifndef PROJ_WT_HELPERS
 #define PROJ_WT_HELPERS
 
