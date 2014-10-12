4e1ff78dd196eeba7ec421b37b9ac500019d6ff9
Marco Gulino <marco@gulinux.net>
2014-10-13 00:37:49 +0200

    refactoring

--- json_object.cpp
+++ json_object.cpp
@@ -24,32 +24,25 @@ string Object::toJson() const {
 }
 
 Wt::Json::Object Object::toWtObject() const {
+    typedef function<void(Wt::Json::Value &, void *)> Exporter;
+    static map<Field::Type, Exporter> exporters {
+        {Field::String, [](Wt::Json::Value &v, void *p){v = Wt::WString::fromUTF8(Value<string>(p)); } },
+        {Field::Int, [](Wt::Json::Value &v, void *p){v = {Value<int>(p)}; } },
+        {Field::LongLong, [](Wt::Json::Value &v, void *p){v = {Value<long long>(p)}; } },
+        {Field::DateTime, [](Wt::Json::Value &v, void *p){v = {boost::posix_time::to_iso_string(Value<boost::posix_time::ptime>(p))}; } },
+        {Field::Object, [](Wt::Json::Value &v, void *p){
+                Wt::Json::Value another(Wt::Json::ObjectType);
+                Object &t = Value<Object>(p);
+                Wt::Json::Object &o = another;
+                o = {t.toWtObject()};
+                v = another;
+            } },
+    };
+
     Wt::Json::Object wtObject;
     for(auto v: fields) {
         Wt::Json::Value value;
-        switch(v.second.type) {
-        case Field::String:
-            //value = {*reinterpret_cast<string*>(v.second.p)};
-            value = {Wt::WString::fromUTF8(Value<string>(v.second.p))};
-            break;
-        case Field::Int:
-            value = {Value<int>(v.second.p)};
-            break;
-        case Field::LongLong:
-            value = {Value<long long>(v.second.p)};
-            break;
-        case Field::DateTime: // TODO: Parameters for format choosing
-            value = {boost::posix_time::to_iso_string(Value<boost::posix_time::ptime>(v.second.p))};
-            break;
-        case Field::Object:
-            Wt::Json::Value another(Wt::Json::ObjectType);
-            Object &t = Value<Object>(v.second.p);
-            Wt::Json::Object &o = another;
-            o = {t.toWtObject()};
-            value = another;
-            break;
-        }
-
+        exporters[v.second.type](value, v.second.p);
         wtObject[v.first] = value;
     }
     return wtObject;
@@ -63,25 +56,20 @@ void Object::fromJson(const std::string &jsonString) {
 }
 
 void Object::from(const Wt::Json::Object &object) {
+    typedef function<void(Wt::Json::Value &, void *)> Importer;
+
+    static map<Field::Type, Importer> importers {
+        {Field::String, [](Wt::Json::Value &v, void *p) { Value<string>(p).set(v); } },
+        {Field::Int, [](Wt::Json::Value &v, void *p) { Value<int>(p).set(v); } },
+        {Field::LongLong, [](Wt::Json::Value &v, void *p) { Value<long long>(p).set(v); } },
+        {Field::DateTime, [](Wt::Json::Value &v, void *p) { Value<boost::posix_time::ptime>(p).set(boost::posix_time::from_iso_string(v)); } },
+        {Field::Object, [](Wt::Json::Value &v, void *p) { Object &object = Value<Object>(p); object.from(v); } },
+    };
+
     for(auto field: fields) {
         auto value = object.at(field.first);
-        switch(field.second.type) {
-        case Field::String:
-            Value<string>(field.second.p).set(value);
-            break;
-        case Field::Int:
-            Value<int>(field.second.p).set(value);
-            break;
-        case Field::LongLong:
-            Value<long long>(field.second.p).set(value);
-            break;
-        case Field::DateTime:
-            Value<boost::posix_time::ptime>(field.second.p).set(boost::posix_time::from_iso_string(value));
-            break;
-        case Field::Object:
-            Object &object = Value<Object>(field.second.p);
-            object.from(value);
-        }
+        importers[field.second.type](value, field.second.p);
+
     }
 }
 
