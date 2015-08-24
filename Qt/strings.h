1c21f009b8a0a6b4de7ee624fc51519df734fda7
Marco Gulino <marco@gulinux.net>
2015-08-24 14:19:20 +0200

    Qt string utilities

---
+++ Qt/strings.h
@@ -0,0 +1,32 @@
+#ifndef QT_COMMONS_STRINGS_H
+#define QT_COMMONS_STRINGS_H
+
+
+#include <QString>
+#include <QDebug>
+#ifdef IN_IDE_PARSER
+#define _q + QString()
+#else
+inline QString operator ""_q(const char *s, std::size_t) { return QString{s}; }
+#endif
+
+template<typename T>
+QString operator%(const QString &other, const T &t) {
+  return other.arg(t);
+}
+
+
+template<>
+inline QString operator%(const QString &first, const std::string &second) {
+  return first.arg(QString::fromStdString(second));
+}
+
+
+inline QDebug operator<<(QDebug dbg, const std::string &str) {
+  dbg << QString::fromStdString(str);
+  return dbg;
+}
+
+
+#endif
+
