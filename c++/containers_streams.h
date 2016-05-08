d0aa29d07c9276d632caff13b71e00f0d099b977
Marco Gulino <marco@gulinux.net>
2016-05-08 18:39:52 +0200

    Add copyright headers

--- c++/containers_streams.h
+++ c++/containers_streams.h
@@ -1,11 +1,9 @@
-#ifndef GULINUX_COMMONS_CONTAINERS_STREAMS
-#define GULINUX_COMMONS_CONTAINERS_STREAMS
 /*
  * Copyright (C) 2016 Marco Gulino <marco@gulinux.net>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,8 +13,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
+
+#ifndef GULINUX_COMMONS_CONTAINERS_STREAMS
+#define GULINUX_COMMONS_CONTAINERS_STREAMS
 #include <utility>
 #include <algorithm>
 #include <functional>
