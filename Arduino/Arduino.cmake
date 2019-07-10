b9e4b43ee858d03617afb6b325ef400a0277288b
Marco Gulino <marco@gulinux.net>
2019-07-10 20:55:15 +0100

    Add ARDUINO_HOME libraries; add Upload method choice

--- Arduino/Arduino.cmake
+++ Arduino/Arduino.cmake
@@ -12,8 +12,10 @@ if("${ARDUINO_HOME}" STREQUAL "")
     message(FATAL_ERROR "Please set the ARDUINO_HOME environment variable")
 endif()
 
+
 if(${BOARD} STREQUAL "bluepill")
-    set(DEVICE_FQBN Arduino_STM32:STM32F1:genericSTM32F103C:device_variant=STM32F103CB,upload_method=DFUUploadMethod,cpu_speed=speed_72mhz,opt=osstd)
+    set(UPLOAD_METHOD DFUUploadMethod CACHE STRING "Options available: DFUUploadMethod, BMPMethod")
+    set(DEVICE_FQBN Arduino_STM32:STM32F1:genericSTM32F103C:device_variant=STM32F103CB,upload_method=${UPLOAD_METHOD},cpu_speed=speed_72mhz,opt=osstd)
     set(TTY ttyACM0 CACHE STRING "Default tty port for picocom (ttyACM0)")
     set(BOARD_EXTRA_OPTS -vid-pid=0X1EAF_0X0004)
 elseif(${BOARD} STREQUAL "nano")
@@ -44,6 +46,7 @@ add_custom_target(
         -tools ${HOME}/.arduino15/packages
         -libraries ${HOME}/Arduino/libraries
         -libraries ${CMAKE_SOURCE_DIR}/libraries
+        -libraries ${ARDUINO_HOME}/libraries
         -fqbn=${DEVICE_FQBN}
         -ide-version=10808
         -build-path ${BUILD_DIR}
